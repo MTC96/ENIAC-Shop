b7ec2204746a218695e4ecd608d013c886717d00
Mateo Castro <mateo@experimentality.co>
2021-03-26 18:51:24 -0500

    configuraciones para heroku, correccion en server.js

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static('./dist/ENIAC-Shop'));
+app.use(express.static('./dist/MercadoLibre'));
 app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname + '/dist/ENIAC-Shop/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/MercadoLibre/index.html'));
 });
 
 app.listen(process.env.PORT || 8080);
